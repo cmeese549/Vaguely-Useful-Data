e693dc21d62d6075f8c45b9df6ddadb8b98cf13b
JEITech <chris@james-enterprise.com>
2020-05-27 14:23:51 -0600

    fixed emails

--- src/modules/user/utils/sendEmail.ts
+++ src/modules/user/utils/sendEmail.ts
@@ -5,10 +5,10 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export async function sendEmail(email: string, token: string) {
     const msg = {
         to: email,
-        from: 'mail@benshapirobuttinjeanspics.',
-        subject: 'VUD Email Stuff',
+        from: 'mail@benshapirobuttinjeanspics.com',
+        subject: 'Vaguely Useful Data',
         text: 'You wanted a thing to happen so click this to make it happen',
-        html: `<a href="${process.env.URL_PRE}${process.env.ENV_URL}/${token}">${process.env.ENV_URL}/${token}</a>`,
+        html: `<a href="${process.env.URL_PRE}${process.env.ENV_URL}/${token}">${process.env.URL_PRE}${process.env.ENV_URL}/${token}</a>`,
       };
       sgMail.send(msg);
 }
